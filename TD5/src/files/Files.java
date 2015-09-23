4b0abbe3dbab0724152f2f26d3ef51d7acd4ec20
MichaelMondou <themikamozart33@live.fr>
2015-09-23 18:51:41 +0200

    Tous les TDs

--- TD5/src/files/Files.java
+++ TD5/src/files/Files.java
@@ -6,7 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-public class Files {
+/*public class Files {
 	public static void main(String[] args) {
 		Charset UTF8 = Charset.forName("UTF-8");
 		Path codeMain = Paths.get("./src/files/CopyBytes.java");
@@ -19,4 +19,4 @@ public class Files {
 			System.err.println("Erreur de lecture : "+ioe);
 		}
 	}
-}
+}*/
