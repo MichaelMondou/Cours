06b0aa62750c012ef839db7e358168135ab34247
MichaelMondou <themikamozart33@live.fr>
2015-10-29 03:36:03 +0100

    Signed-off-by: MichaelMondou <themikamozart33@live.fr>

---
+++ Java/Temperatures_final/src/mvc/Window.java
@@ -0,0 +1,42 @@
+package mvc;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
+
+/**
+ * Fenêtre principale de l'application
+ */
+@SuppressWarnings("serial")
+public class Window extends JFrame {
+
+	/**
+	 * Fenêtre
+	 */
+	private final JFrame window;
+	/**
+	 * Bureau contenant les éléments de l'application
+	 */
+	private final JDesktopPane desktop;
+
+	public Window() {
+		window = new JFrame();
+		window.setTitle("Conversions de températures");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setSize(650, 400);
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
+
+		desktop = new JDesktopPane();
+		window.add(desktop, BorderLayout.CENTER);
+	}
+
+	public JFrame getWindow() {
+		return window;
+	}
+
+	public JDesktopPane getDesktop() {
+		return desktop;
+	}
+}
