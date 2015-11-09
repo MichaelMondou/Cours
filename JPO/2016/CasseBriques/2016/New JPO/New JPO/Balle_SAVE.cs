817aa22ef0d0676bba8da1e26e476edd3c6af65a
remimomprive <remi.momprive@gmail.com>
2015-11-09 19:31:26 +0100

    Signed-off-by: remimomprive <remi.momprive@gmail.com>

---
+++ JPO/2016/CasseBriques/2016/New JPO/New JPO/Balle_SAVE.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace New_JPO
+{
+    class BalleSave : PictureBox
+    {
+        private double deplacementX, deplacementY;
+        private bool toucheBloc = false; // vrai si la balle a touché un bloc au tour actuel
+        private Point Centre;
+
+        public bool ToucheBloc
+        {
+            get { return toucheBloc; }
+            set { toucheBloc = value; }
+        }
+
+        //Initialisation de la balle
+        public BalleSave()
+        {
+            this.BackColor = Constantes.COULEUR_BALLE;
+            this.Size = new Size(Constantes.TAILLE_BALLE, Constantes.TAILLE_BALLE);
+            this.Centre = new Point(this.Location.X + ((this.Location.X + this.Width - this.Location.X) / 2), (this.Location.Y + (this.Location.Y + this.Height - this.Location.Y) / 2));
+            initialisation();
+        }
+
+        public void initialisation()
+        {
+            deplacementX = Constantes.VITESSE_BALLE;
+            deplacementY = -Constantes.VITESSE_BALLE;
+            this.Location = new Point(978 / 2 - (Constantes.TAILLE_BALLE / 2), 480);
+        }
+
+        public void miseAJourNiveau(Niveau niveau_du_jeu)
+        {
+            switch (niveau_du_jeu)
+            {
+                case Niveau.DEBUTANT:
+                    if (deplacementX > 0)
+                        deplacementX = Constantes.VITESSE_BALLE;
+                    else
+                        deplacementX = -Constantes.VITESSE_BALLE;
+                    if (deplacementY > 0)
+                        deplacementY = Constantes.VITESSE_BALLE;
+                    else
+                        deplacementY = -Constantes.VITESSE_BALLE;
+                    break;
+                case Niveau.INTERMEDIAIRE:
+                    if (deplacementX > 0)
+                        deplacementX = Constantes.VITESSE_BALLE * 1.5;
+                    else
+                        deplacementX = -Constantes.VITESSE_BALLE * 1.5;
+                    if (deplacementY > 0)
+                        deplacementY = Constantes.VITESSE_BALLE * 1.5;
+                    else
+                        deplacementY = -Constantes.VITESSE_BALLE * 1.5;
+                    break;
+                case Niveau.EXPERT:
+                    if (deplacementX > 0)
+                        deplacementX = Constantes.VITESSE_BALLE * 2.0;
+                    else
+                        deplacementX = -Constantes.VITESSE_BALLE * 2.0;
+                    if (deplacementY > 0)
+                        deplacementY = Constantes.VITESSE_BALLE * 2.0;
+                    else
+                        deplacementY = -Constantes.VITESSE_BALLE * 2.0;
+                    break;
+            }
+        }
+
+        public void bouger()
+        {
+            Location = new Point(Location.X + (int)deplacementX, Location.Y + (int)deplacementY);
+            this.Centre = new Point(this.Location.X + ((this.Location.X + this.Width - this.Location.X) / 2), (this.Location.Y + (this.Location.Y + this.Height - this.Location.Y) / 2));
+        }
+
+        public void toucherFenetre(int largeurFenetre, int hauteurFenetre) // Savoir si la balle sort de la fenêtre
+        {
+            if (Location.X + Constantes.TAILLE_BALLE >= largeurFenetre - Constantes.TAILLE_BALLE)
+            {
+                deplacementX = -1 * deplacementX;
+            }
+            if (Location.X < 0)
+            {
+                deplacementX = -1 * deplacementX;
+            }
+            if (Location.Y < 25)
+            {
+                deplacementY = -1 * deplacementY;
+            }
+        }
+
+        public void toucherBarre(Barre barre) // Savoir si la balle touche la barre
+        {
+            if (this.Location.Y + this.Size.Height > barre.Location.Y &&
+               this.Location.Y < barre.Location.Y &&
+               this.Location.X + this.Size.Width > barre.Location.X &&
+               this.Location.X < barre.Location.X + barre.Size.Width)
+            {
+                deplacementY = -1 * deplacementY;
+            }
+        }
+
+        public bool sortie()
+        {
+            if (Location.Y > 550)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int toucheDuBloc(Bloc bloc)
+        {
+            int nb = 0;
+            /**
+             *Verifie si un des coins de la balle rentre dans la brique
+             **/
+            if (coinSupDroit(bloc) || CoinSupGauche(bloc) || CoinInfGauche(bloc) || CoinInfDroit(bloc))
+            {
+                /**
+                * Si le centre de la balle est à gauche ou à droite de la brique
+                **/
+
+                if ((this.Centre.Y <= bloc.Location.Y) || (this.Centre.Y >= bloc.Location.Y + bloc.Height))
+                {
+                    deplacementY = -1 * deplacementY;
+                }
+                else if ((this.Centre.X <= bloc.Location.X) || (this.Centre.X >= bloc.Location.X + bloc.Width))
+                {
+                    deplacementX = -1 * deplacementX;
+                }
+
+                nb = Constantes.SCORE_BRIQUE;
+                bloc.Visible = false;
+                ToucheBloc = true;
+            }
+            return nb;
+        }
+
+        private bool CoinInfDroit(Bloc bloc)
+        {
+            bool rentre = false;
+            if (((bloc.Location.Y <= this.Location.Y + this.Height) && (this.Location.Y + this.Height <= bloc.Location.Y + bloc.Height)) &&
+                ((bloc.Location.X <= this.Location.X + this.Width) && (this.Location.X <= bloc.Location.X + bloc.Width)))
+            {
+                rentre = true;
+            }
+            return rentre;
+        }
+
+        private bool CoinInfGauche(Bloc bloc)
+        {
+            bool rentre = false;
+            if (((bloc.Location.Y <= this.Location.Y + this.Height) && (this.Location.Y + this.Height <= bloc.Location.Y + bloc.Height)) &&
+                ((bloc.Location.X <= this.Location.X) && (this.Location.X <= bloc.Location.X + bloc.Width)))
+            {
+                rentre = true;
+            }
+            return rentre;
+        }
+
+        private bool CoinSupGauche(Bloc bloc)
+        {
+            bool rentre = false;
+            if (((bloc.Location.Y <= this.Location.Y) && (this.Location.Y <= bloc.Location.Y + bloc.Height)) &&
+                ((bloc.Location.X <= this.Location.X) && (this.Location.X <= bloc.Location.X + bloc.Width)))
+            {
+                rentre = true;
+            }
+            return rentre;
+        }
+
+        private bool coinSupDroit(Bloc bloc)
+        {
+            bool rentre = false;
+            if (((bloc.Location.Y <= this.Location.Y) && (this.Location.Y <= bloc.Location.Y + bloc.Height)) &&
+                ((bloc.Location.X <= this.Location.X + this.Width) && (this.Location.X <= bloc.Location.X + bloc.Width)))
+            {
+                rentre = true;
+            }
+            return rentre;
+        }
+
+        public double DeplacementY
+        {
+            get { return deplacementY; }
+            set { deplacementY = value; }
+        }
+
+        public double DeplacementX
+        {
+            get { return deplacementX; }
+            set { deplacementX = value; }
+        }
+
+
+    }
+}
---
+++ JPO/2016/CasseBriques/2016/New JPO/New JPO/JPO.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace New_JPO
+{
+    partial class JPO
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(JPO));
+            this.Barre_Menu = new System.Windows.Forms.MenuStrip();
+            this.jeuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.niveauToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.débutantToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.intermédiaireToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.expertToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Barre_Menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Barre_Menu
+            // 
+            this.Barre_Menu.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Barre_Menu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.Barre_Menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.jeuToolStripMenuItem,
+            this.niveauToolStripMenuItem});
+            this.Barre_Menu.Location = new System.Drawing.Point(0, 0);
+            this.Barre_Menu.MinimumSize = new System.Drawing.Size(0, 28);
+            this.Barre_Menu.Name = "Barre_Menu";
+            this.Barre_Menu.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.Barre_Menu.Size = new System.Drawing.Size(962, 28);
+            this.Barre_Menu.TabIndex = 0;
+            this.Barre_Menu.Text = "Barre_Menu";
+            // 
+            // jeuToolStripMenuItem
+            // 
+            this.jeuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quitterToolStripMenuItem});
+            this.jeuToolStripMenuItem.Name = "jeuToolStripMenuItem";
+            this.jeuToolStripMenuItem.Size = new System.Drawing.Size(36, 24);
+            this.jeuToolStripMenuItem.Text = "Jeu";
+            // 
+            // quitterToolStripMenuItem
+            // 
+            this.quitterToolStripMenuItem.Name = "quitterToolStripMenuItem";
+            this.quitterToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.quitterToolStripMenuItem.Text = "Quitter";
+            this.quitterToolStripMenuItem.Click += new System.EventHandler(this.quitterToolStripMenuItem_Click);
+            // 
+            // niveauToolStripMenuItem
+            // 
+            this.niveauToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.débutantToolStripMenuItem,
+            this.intermédiaireToolStripMenuItem,
+            this.expertToolStripMenuItem});
+            this.niveauToolStripMenuItem.Name = "niveauToolStripMenuItem";
+            this.niveauToolStripMenuItem.Size = new System.Drawing.Size(56, 24);
+            this.niveauToolStripMenuItem.Text = "Niveau";
+            // 
+            // débutantToolStripMenuItem
+            // 
+            this.débutantToolStripMenuItem.Name = "débutantToolStripMenuItem";
+            this.débutantToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.débutantToolStripMenuItem.Text = "Débutant";
+            this.débutantToolStripMenuItem.Click += new System.EventHandler(this.débutantToolStripMenuItem_Click);
+            // 
+            // intermédiaireToolStripMenuItem
+            // 
+            this.intermédiaireToolStripMenuItem.Name = "intermédiaireToolStripMenuItem";
+            this.intermédiaireToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.intermédiaireToolStripMenuItem.Text = "Intermédiaire";
+            this.intermédiaireToolStripMenuItem.Click += new System.EventHandler(this.intermédiaireToolStripMenuItem_Click);
+            // 
+            // expertToolStripMenuItem
+            // 
+            this.expertToolStripMenuItem.Name = "expertToolStripMenuItem";
+            this.expertToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.expertToolStripMenuItem.Text = "Expert";
+            this.expertToolStripMenuItem.Click += new System.EventHandler(this.expertToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(12, 506);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Score";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30.25F);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Location = new System.Drawing.Point(400, 332);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 47);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "PAUSE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label3.Location = new System.Drawing.Point(533, 513);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 22);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Indications";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.8F);
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label4.Location = new System.Drawing.Point(180, 505);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 30);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Vies";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.8F);
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label5.Location = new System.Drawing.Point(332, 288);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(287, 44);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "FIN DE PARTIE";
+            // 
+            // JPO
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(962, 547);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Barre_Menu);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.Barre_Menu;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "JPO";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "JPO";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.JPO_KeyDown);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.JPO_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.JPO_MouseMove);
+            this.Barre_Menu.ResumeLayout(false);
+            this.Barre_Menu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip Barre_Menu;
+        private System.Windows.Forms.ToolStripMenuItem jeuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitterToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ToolStripMenuItem niveauToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem débutantToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem intermédiaireToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem expertToolStripMenuItem;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+
+    }
+}
+
