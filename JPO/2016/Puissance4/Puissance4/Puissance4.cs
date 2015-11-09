840fbfd4b4208e1eb1c64bc939d4431587d732b2
remimomprive <remi.momprive@gmail.com>
2015-11-09 23:00:00 +0100

    Puissance 4 version Visual Studio 2013

---
+++ JPO/2016/Puissance4/Puissance4/Puissance4.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Puissance4
+{
+    public partial class Puissance4 : Form
+    {
+        public static int SIZE_W = 97;
+        public static int SIZE_H = 97;
+
+        public static int NB_COLS = 7;//7
+        public static int NB_ROWS = 6;//6
+
+        public static int MARGIN_TOP = 25;//Marge du haut (taille du menu en px)
+        public static int MARGIN_BOTTOM = 25;//Marge du bas (taille de la barre en px)
+
+        public static int WIDTH = SIZE_W * NB_COLS;
+        public static int HEIGHT = SIZE_H * NB_ROWS;
+
+        public static int SPEED = 25;
+
+        public static int PUISSANCE = 4;
+
+        private Grille grille;
+        private Jeton jeton;//Jeton que l'on déplace en haut de la grille
+        private Point[] jetons_gagnants;
+
+        //Nombre de victoire des joueurs
+        private int joueurRouge = 0;
+        private int joueurJaune = 0;
+
+        private String joueur = "rouge";//Couleur du joueur qui doit jouer
+
+        private int nbJetons = 0;//Nombre de jeton posés
+
+        public Puissance4()
+        {
+            #region Puissance4
+            InitializeComponent();
+
+            ClientSize = new System.Drawing.Size(WIDTH - 6, HEIGHT + SIZE_H + MARGIN_TOP + MARGIN_BOTTOM - 6);
+
+            this.grille = new Grille();
+            this.jeton = new Jeton(joueur, WIDTH / 2 - SIZE_W / 2, 0);
+            #endregion
+
+            toolStripStatusLabel1.Text = "Rouge : 0";
+            toolStripStatusLabel2.Text = "Jaune : 0";
+
+            Refresh();
+            
+        }
+
+        private void init()
+        {
+            grille.init();
+
+            joueur = "rouge";
+
+            jeton.setCouleur(joueur);
+            jetons_gagnants = null;
+
+            nbJetons = 0;
+
+            Refresh();
+        }
+
+        private void Puissance4_Paint(object sender, PaintEventArgs e)
+        {
+            jeton.dessiner(e.Graphics);
+            grille.dessiner(e.Graphics);
+
+            if (jetons_gagnants != null)
+            {
+                Jeton.dessinerTrait(e.Graphics, jetons_gagnants);
+            }
+        }
+
+        private void Puissance4_MouseMove(object sender, MouseEventArgs e)
+        {
+            int x = (e.X / SIZE_W) * SIZE_W;
+
+            if (x < 0)
+            {
+                x = 0;
+            }
+            else if (x > WIDTH - SIZE_W)
+            {
+                x = WIDTH - SIZE_W;
+            }
+
+            jeton.setPotision(x, 0);
+            Refresh();
+        }
+
+        private void nouveauToolStripButton_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Voulez-vous commencer une nouvelle partie ?", "Nouvelle partie", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                joueurRouge = 0;
+                joueurJaune = 0;
+                toolStripStatusLabel1.Text = "Rouge : 0";
+                toolStripStatusLabel2.Text = "Jaune : 0";
+                init();
+            }
+        }
+
+        private void AboutButton_Click(object sender, EventArgs e)
+        {
+            Apropos about = new Apropos();
+            about.ShowDialog();
+        }
+
+        private void Puissance4_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Puissance4_MouseClick(object sender, MouseEventArgs e)
+        {
+            #region MouseCLick
+            int i = ((MouseEventArgs)e).X / SIZE_W;
+
+            if (i >= NB_COLS)
+            {
+                i = NB_COLS - 1;
+            }
+
+            if (grille[i, 0].getCouleur() != null)
+            {
+                return;
+            }
+            int j = grille.ligneInsertion(i);
+            int y = 0;
+
+            while (y <= HEIGHT - (NB_ROWS - j - 1) * SIZE_H)
+            {
+                jeton.setPositionY(y);
+                y++;
+
+                if (y % SPEED == 0)
+                {
+                    Refresh();
+                }
+            }
+
+            grille[i, j].setCouleur(jeton.getCouleur());
+            Puissance4_MouseMove(sender, (MouseEventArgs)e);
+            jeton.inverserCouleur();
+
+            Refresh();
+            #endregion
+
+            jetons_gagnants = grille.jetonGagnant(i, j);
+            if (jetons_gagnants != null)
+            {
+                Refresh();//Permet d'afficher quels jetons sont gagnants
+
+                MessageBox.Show("Partie finie !\nVictoire du joueur " + joueur);
+
+                if (joueur == "rouge")
+                {
+                    joueurRouge++;
+                    toolStripStatusLabel1.Text = "Rouge : " + joueurRouge.ToString();
+                }
+                else if (joueur == "jaune")
+                {
+                    joueurJaune++;
+                    toolStripStatusLabel2.Text = "Jaune : " + joueurJaune.ToString();
+                }
+
+                init();
+            }
+            else if (++nbJetons == NB_COLS * NB_ROWS)
+            {
+                MessageBox.Show("Egalité !");
+                joueurRouge++;
+                joueurJaune++;
+                toolStripStatusLabel1.Text = "Rouge : " + joueurRouge.ToString();
+                toolStripStatusLabel2.Text = "Jaune : " + joueurJaune.ToString();
+
+                init();
+            }
+            else
+            {
+                if (joueur == "rouge")
+                {
+                    joueur = "jaune";
+                }
+                else if (joueur == "jaune")
+                {
+                    joueur = "rouge";
+                }
+            }
+        }
+    }
+}
---
+++ JPO/2016/Puissance4_/Puissance 4/Grille.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Puissance_4
+{
+    class Grille
+    {
+        private Jeton[][] jetons = new Jeton[Puissance4.NB_COLS][];
+
+        public Grille()
+        {
+            init();
+        }
+
+        public void init()
+        {
+            for (int i = 0; i < Puissance4.NB_COLS; i++)
+            {
+                this.jetons[i] = new Jeton[Puissance4.NB_COLS];
+
+                for (int j = 0; j < Puissance4.NB_COLS; j++)
+                {
+                    this.jetons[i][j] = new Jeton(null, i * Puissance4.SIZE_W, j * Puissance4.SIZE_H + Puissance4.SIZE_H);
+                }
+            }
+        }
+
+        public void dessiner(Graphics g)
+        {
+            for (int i = 0; i < Puissance4.NB_COLS; i++)
+                for (int j = 0; j < Puissance4.NB_ROWS; j++)
+                {
+                    dessinerJetons(g, i, j);
+                    dessinerCase(g, i, j);
+                }
+        }
+
+        private void dessinerJetons(Graphics g, int i, int j)
+        {
+            this.jetons[i][j].dessiner(g);
+        }
+
+        private void dessinerCase(Graphics g, int x, int y)
+        {
+            Image image = Properties.Resources._case;
+            g.DrawImage(image, new Rectangle(x * Puissance4.SIZE_W, Puissance4.MARGIN_TOP + y * Puissance4.SIZE_H + Puissance4.SIZE_H, Puissance4.SIZE_W, Puissance4.SIZE_H));
+        }
+
+        public int ligneInsertion(int i)
+        {
+            int j = 0;
+
+            while (j < Puissance4.NB_ROWS && this.jetons[i][j].getCouleur() == null)
+            {
+                j++;
+            }
+
+            return j - 1;
+        }
+
+        public Jeton this[int i, int j]
+        {
+            get
+            {
+                return this.jetons[i][j];
+            }
+
+            set
+            {
+                this.jetons[i][j] = value;
+            }
+        }
+
+        public Point[] jetonGagnant(int i, int j)
+        {
+            Point[] jetons;
+
+            if ((jetons = verifierColonne(i, j)) != null)
+            {
+                return jetons;
+            }
+            else if ((jetons = verifierLigne(i, j)) != null)
+            {
+                return jetons;
+            }
+            else if ((jetons = verifierDiagonaleNO_SE(i, j)) != null)
+            {
+                return jetons;
+            }
+            else if ((jetons = verifierDiagonaleNE_SO(i, j)) != null)
+            {
+                return jetons;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private Point[] verifierColonne(int col, int lig)
+        {
+            String couleur = this.jetons[col][lig].getCouleur();
+
+            int j = lig + 1;
+            int cpt = 1;
+
+            if (lig <= Puissance4.NB_ROWS - Puissance4.PUISSANCE)
+            {
+                while (j < Puissance4.NB_ROWS && couleur == this.jetons[col][j++].getCouleur())
+                {
+                    cpt++;
+                }
+
+                if (cpt >= Puissance4.PUISSANCE)
+                {
+                    Point p1 = new Point(col, lig);
+                    Point p2 = new Point(col, lig + Puissance4.PUISSANCE - 1);
+
+                    return new Point[] { p1, p2 };
+                }
+            }
+
+            return null;
+        }
+
+        private Point[] verifierLigne(int col, int lig)
+        {
+            String couleur = this.jetons[col][lig].getCouleur();
+
+            int cpt = 1;
+            
+            Point p1 = new Point(col, lig);
+            Point p2 = new Point(col, lig);
+
+            //Comptage à droite
+            int i = col;
+            while (i < Puissance4.NB_COLS - 1 && couleur == this.jetons[++i][lig].getCouleur())
+            {
+                p2.X = i;
+                cpt++;
+            }
+
+            //Comptage à gauche
+            i = col;
+            while (i > 0 && couleur == this.jetons[--i][lig].getCouleur())
+            {
+                p1.X = i;
+                cpt++;
+            }
+
+            if (cpt >= Puissance4.PUISSANCE)
+            {
+                return new Point[] { p1, p2 };
+            }
+
+            return null;
+        }
+
+        private Point[] verifierDiagonaleNO_SE(int col, int lig)
+        {
+            String couleur = this.jetons[col][lig].getCouleur();
+
+            int cpt = 1;
+            
+            Point p1 = new Point(col, lig);
+            Point p2 = new Point(col, lig);
+
+            //Vers le bas
+            int i = col;
+            int j = lig;
+            while (i < Puissance4.NB_COLS - 1 && j < Puissance4.NB_ROWS - 1 && couleur == this.jetons[++i][++j].getCouleur())
+            {
+                p1.X = i;
+                p1.Y = j;
+                cpt++;
+            }
+                        
+            //Vers le haut
+            i = col;
+            j = lig;
+            while (i > 0 && j > 0 && couleur == this.jetons[--i][--j].getCouleur())
+            {
+                p2.X = i;
+                p2.Y = j ;
+                cpt++;
+            }
+
+            if (cpt >= Puissance4.PUISSANCE)
+            {
+                return new Point[] { p1, p2 };
+            }         
+
+            return null;
+        }
+
+        private Point[] verifierDiagonaleNE_SO(int col, int lig)
+        {
+            String couleur = this.jetons[col][lig].getCouleur();
+
+            int cpt = 1;
+            
+            Point p1 = new Point(col, lig);
+            Point p2 = new Point(col, lig);
+
+            //Vers le bas
+            int i = col;
+            int j = lig;
+            while (i > 0 && j < Puissance4.NB_ROWS - 1 && couleur == this.jetons[--i][++j].getCouleur())
+            {
+                p1.X = i;
+                p1.Y = j;
+                cpt++;
+            }
+
+            //Vers le haut
+            i = col;
+            j = lig;
+            while (i < Puissance4.NB_COLS - 1 && j > 0 && couleur == this.jetons[++i][--j].getCouleur())
+            {
+                p2.X = i;
+                p2.Y = j;
+                cpt++;
+            }
+
+            if (cpt >= Puissance4.PUISSANCE)
+            {
+                return new Point[] { p1, p2 };
+            }
+
+            return null;
+        }
+    }
+}
