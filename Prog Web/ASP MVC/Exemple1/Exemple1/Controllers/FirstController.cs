f3fc37e73c82b7dc60a9e675f569969d632a263f
MichaelMondou <themikamozart33@live.fr>
2015-11-09 10:46:15 +0100

    Signed-off-by: MichaelMondou <themikamozart33@live.fr>

---
+++ Prog Web/ASP MVC/Exemple1/Exemple1/Controllers/FirstController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exemple1.Controllers
+{
+    public class FirstController : Controller
+    {
+        // GET: First
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ContentResult TestContent()
+        {
+            string data = "<h1>Test de Content Result.</h1>";
+            return Content(data, "text/html");
+        }
+
+        public string TestString()
+        {
+            string txt = "Hello world !";
+            return txt;
+        }
+
+        public EmptyResult TestEmpty()
+        {
+            return null;
+        }
+    }
+}
---
+++ Prog Web/ASP MVC/Exemple1/Exemple1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Exemple1.MvcApplication" Language="C#" %>
